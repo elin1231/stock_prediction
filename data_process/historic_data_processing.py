fccf82704ca2d1fd455ffc54a623f6d2e665507e
elin1231 <43862267+elin1231@users.noreply.github.com>
2020-06-06 18:41:33 -0500

    Update historic_data_processing.py

--- data_process/historic_data_processing.py
+++ data_process/historic_data_processing.py
@@ -5,10 +5,9 @@ import yfinance as yf
 import numpy as np
 from ta import add_all_ta_features
 from multiprocessing import Pool, Manager
-
+import time
 
 historial_data_path = "./data/historical_data"
-
 # For some reason these files are all empty
 
 
@@ -47,15 +46,17 @@ def add_general_info(df, ticker):
             try:
                 company_info = yf.Ticker(ticker).info
                 df[column] = company_info[column]
+                print("SUCCESS: {}".format(ticker))
             except:
+                print("NO GENERAL INFO FOR ticker:{} column:{}".format(ticker, column))
                 df[column] = np.nan
-                continue
 
 
 def add_technical_info(df, ticker):
     try:
-        df = add_all_ta_features(df, open="Open", high="High",
-                                 low="Low", close="Close", volume="Volume")
+        df = add_all_ta_features(
+            df, open="Open", high="High", low="Low", close="Close", volume="Volume"
+        )
     except:
         return
 
@@ -70,7 +71,7 @@ def run(file):
             df = pd.read_csv(csv_file)
             if len(df.index) > 1:
                 # Add whatever method you want to do for historical data, all should be in their own method
-                add_technical_info(df, ticker)
+                # add_technical_info(df, ticker)
                 # add_mean(df)
                 add_general_info(df, ticker)
                 df.to_csv(os.path.join(historial_data_path, file), index=False)
@@ -79,5 +80,5 @@ def run(file):
 if __name__ == "__main__":
     remove_empty_files()
     files_list = os.listdir(historial_data_path)
-    with Pool(30) as p:
+    with Pool(3) as p:
         p.map(run, files_list)
